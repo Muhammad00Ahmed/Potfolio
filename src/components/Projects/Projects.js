caf455f83b9ab576f65cf8aae75a64ec3973aef2
Muhammad Ahmed Omar <155357806+Muhammad00Ahmed@users.noreply.github.com>
2025-07-05 23:31:33 +0500

    Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="Neer Drop"
               description="Neer Drop is a water-based brand marketing agency designed to help businesses make a splash in the market. Inspired by the idea of clean, bold impact like a drop of water, Neer Drop specializes in creative ad campaigns, branded merchandise, and guerrilla marketing that leave a lasting impression."
-              ghLink="https://github.com/MuhammadAhmed4419/Chatify"
+           
               demoLink="https://roaring-starburst-da29f7.netlify.app/"
             />
           </Col>
@@ -38,7 +38,7 @@ function Projects() {
               isBlog={false}
               title="Meta Store.Ai"
               description="Meta Store.AI is an AI-powered website builder that helps you launch ecommerce stores in minutes—just like Shopify, but smarter. It uses intelligent templates and automation to generate fully functional online shops tailored to your niche, products, and branding—no coding required."
-              ghLink="https://github.com/MuhammadAhmed4419/Bits-0f-C0de"
+             
               demoLink="https://stalwart-starlight-96adb1.netlify.app/"
             />
           </Col>
@@ -50,7 +50,6 @@ function Projects() {
               title="PixForge AI"
               description="PixForge AI is a powerful image and video editing suite built for creators. It offers AI-enhanced tools for editing, enhancing, and transforming media with ease. From background removal and color grading to smart filters and real-time previews, PixForge AI empowers users to produce studio-quality content in the browser.
 "
-              ghLink="https://github.com/MuhammadAhmed4419/Editor.io"
               demoLink="https://imaginative-jalebi-967878.netlify.app/"
             />
           </Col>
